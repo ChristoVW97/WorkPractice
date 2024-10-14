d63fd1e837d9b3760b7f54b2e55634616a53d304
Christo van Wyk <christovanwyk00@gmail.com>
2024-10-14 13:19:38 +0200

    Created Model, DB and Basic Configuration

---
+++ Work/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Work.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                
+        }
+    }
+}
---
+++ Work/Models/Employees.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Work.Models
+{
+    public class Employees
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
